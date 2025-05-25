bd7d0063612d0dccffea281fa77b4245078c5f64
Yana-Rayeva <rayevayana27@gmail.com>
2025-05-26 01:27:16 +0300

    Release notes

--- SQL/Test/CRM_Restaurant_test_4.sql
+++ SQL/Test/CRM_Restaurant_test_4.sql
@@ -5,7 +5,7 @@ WITH generated_orders AS (
         (floor(random() * 7) + 1)::INTEGER AS id_table,
         (floor(random() * 6))::INTEGER AS id_promocode,
         (TIMESTAMP '2023-01-01 00:00:00' + 
-         (floor(random() * (EXTRACT(EPOCH FROM TIMESTAMP '2025-12-31 23:59:59') - 
+        (floor(random() * (EXTRACT(EPOCH FROM TIMESTAMP '2025-12-31 23:59:59') - 
          EXTRACT(EPOCH FROM TIMESTAMP '2023-01-01 00:00:00'))) * INTERVAL '1 second')) AS open_datetime,
         (ARRAY[100, 110, 120])[floor(random() * 3 + 1)] AS system_status,
         (ARRAY[210, 220, 230, 240, 250, 260, 270, 280, 290, 291])[floor(random() * 10 + 1)] AS work_status,
@@ -13,14 +13,16 @@ WITH generated_orders AS (
     FROM generate_series(1, 10000) AS s
 )
 
-INSERT INTO client_orders (id_staffshift, id_client, id_table, id_promocode, open_datetime, close_datetime, order_number, system_status, work_status, paid_status)
+INSERT INTO client_orders (id_staffshift, id_client, id_table, id_promocode, open_datetime, close_datetime, 
+order_number, system_status, work_status, paid_status)
 SELECT 
     id_staffshift,
     id_client,
     id_table,
     id_promocode,
     open_datetime,
-    (open_datetime + (INTERVAL '10 minutes' + (floor(random() * (240 - 10 + 1)) * INTERVAL '1 minute')) + (floor(random() * 240) * INTERVAL '1 minute')) AS close_datetime,
+    (open_datetime + (INTERVAL '10 minutes' + (floor(random() * (240 - 10 + 1)) * INTERVAL '1 minute')) + 
+    (floor(random() * 240) * INTERVAL '1 minute')) AS close_datetime,
     (id_client::TEXT || '-' || (floor(random() * (10000 - 1000 + 1)) + 1000)::TEXT) AS order_number,
     system_status,
     work_status,
